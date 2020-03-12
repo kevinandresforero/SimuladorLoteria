7739b5a546b4e57e261d8baea332de7f19e83abc
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2020-03-12 12:43:53 -0500

    0/5

---
+++ Loteria.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import tkinter as tk
+#from tkinter import *
+
+ventana= tk.Tk()
+ventana.title("Loteria")
+ventana.geometry("500x500")
+ventana.configure(bg="black")
+
+
+
+ventana.mainloop()
+
+"""
+Created on Thu Mar 12 09:42:28 2020
+@author: Kevin
+"""
+
