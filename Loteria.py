f6b23137dca4234b70b928eb87e5e03eb5a0d4f3
dragesteban <54458821+dragesteban@users.noreply.github.com>
2020-03-16 18:41:30 -0500

    Update Loteria.py

--- Loteria.py
+++ Loteria.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from random import randint
 import tkinter as tk
 #from tkinter import *
 
@@ -7,10 +9,21 @@ ventana= tk.Tk()
 ventana.title("Loteria")
 ventana.geometry("500x500")
 ventana.configure(bg="black")
-
-
-
 ventana.mainloop()
+
+comparador = randint(1000000, 9999999)
+
+numero = int(input("Escriba el número para la lotería: "))
+
+i = 1
+
+while comparador != numero:
+    comparador = randint(1000000, 9999999)
+    i = i + 1
+    print("Comparador: ", comparador)
+    print("i: ", i)
+
+print("Ganó en el intento: ", i, " con el número: ", comparador)
 
 """
 Created on Thu Mar 12 09:42:28 2020
