3004bcdb0af845be68b1919a6b044b9e80c48919
dragesteban <54458821+dragesteban@users.noreply.github.com>
2020-03-16 22:31:38 -0500

    Update Loteria.py

--- Loteria.py
+++ Loteria.py
@@ -3,29 +3,55 @@
 
 from random import randint
 import tkinter as tk
-#from tkinter import *
+from tkinter import *
 
-ventana= tk.Tk()
-ventana.title("Loteria")
-ventana.geometry("500x500")
-ventana.configure(bg="black")
-ventana.mainloop()
-
-comparador = randint(1000000, 9999999)
-
-numero = int(input("Escriba el número para la lotería: "))
-
-i = 1
-
-while comparador != numero:
+def consultar():
     comparador = randint(1000000, 9999999)
-    i = i + 1
-    print("Intento: ", i)
+    
+    numero = int(campo1.get())
+    
+    i = 1
+    
+    while comparador != numero:
+        comparador = randint(1000000, 9999999)
+        i = i + 1
+        contador.set(i)
+        print(i)
+    
+    resultado.set(f"Ganó en el intento {i} con el número: {comparador}")
 
-print("Ganó en el intento: ", i, " con el número: ", comparador)
+def cerrar():
+    marco.destroy()
+
+marco = tk.Tk()
+marco.title("Simulador de Loteria")
+marco.geometry("500x400")
+marco.configure(bg="black")
+
+contador = tk.StringVar()
+resultado = tk.StringVar()
+
+l1 = tk.Label(marco, text="Ingrese numero de la loteria: ", bg="black", fg="red").grid(row=0, column=0)
+l2 = tk.Label(marco, textvariable=resultado, bg="black", fg="red").grid(row=2, column=1)
+l3 = tk.Label(marco, textvariable=contador, bg="black", fg="red").grid(row=2, column=0)
+
+campo1 = tk.Entry(marco, bg="red", fg="black")
+campo1.grid(row=0, column=1)
+
+tk.Button(marco, text='Consultar', command=consultar, bg="black", fg="red").grid(row=0,
+                                                                                 column=2,
+                                                                                 sticky=tk.W,
+                                                                                 pady=4)
+
+tk.Button(marco, text='Cerrar', command=cerrar, bg="black", fg="red").grid(row=3,
+                                                                           column=0,
+                                                                           sticky=tk.W,
+                                                                           pady=4)
+
+marco.mainloop()
 
 """
 Created on Thu Mar 12 09:42:28 2020
-@author: Kevin
+@author: Dragesteban
 """
 
